ffd6a638c7294cd5f6d4c470f24ab26117ed5df2
duybluemind1988 <bluemind1988@gmail.com>
2021-02-04 18:54:28 +0700

    add

--- Job_attritiion/Shiny_app/Job_attrition_app.R
+++ Job_attritiion/Shiny_app/Job_attrition_app.R
@@ -4,6 +4,7 @@ library(shiny)
 library(scales)
 library(rsample) 
 library(caret)
+library(h2o)
 
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 path <- "WA_Fn-UseC_-HR-Employee-Attrition.csv"
@@ -22,8 +23,12 @@ ui <- fluidPage(
       dataTableOutput("data_head_DT"),
       selectInput("cat_compare", "Choose category column to compare", choices=NULL, selected=NULL),
       plotOutput("cat_vs_cat_chart"),
+      plotOutput("cat_vs_cat_chart2"),
       selectInput("num_compare", "Choose numeric column to compare", choices=NULL, selected=NULL),
       plotOutput("cat_vs_num_chart"),
+      plotOutput("density_chart"),
+      "Variable important H2O model:",
+      plotOutput("variable_important"),
     )#end mainpanel
   )# end sidebarlayout
 )
@@ -56,9 +61,24 @@ server <- function(input, output, session) {
       scale_fill_brewer(palette = "Set2") +
       #labs(y = "Percent",fill = "Drive Train",x = "Class",title = "Automobile Drive by Class") +
       theme_minimal() 
+  # Plot categorical vs categorical 2  
+  })
+  output$cat_vs_cat_chart2 <- renderPlot({
+    data() %>%
+      ggplot(aes_(x = input$cat_compare, group = ~Attrition)) + 
+      geom_bar(aes(y = ..prop.., fill = factor(..x..)), 
+               stat="count", 
+               alpha = 0.7) +
+      geom_text(aes(label = scales::percent(..prop..), y = ..prop.. ), 
+                stat= "count", 
+                vjust = 2) +
+      #labs(y = "Percentage", fill= "Education") +
+      facet_grid(~Attrition) +
+      theme_minimal()+
+      theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + 
+      ggtitle("Attrition") 
     
   })
-  
   # Plot Categorical vs. Quantitative
   output$cat_vs_num_chart<-renderPlot({
     #Categorical_vs_quantitative_plot(data(),~Attrition,input$num_compare)
@@ -71,6 +91,25 @@ server <- function(input, output, session) {
                    outlier.color = "orange",
                    outlier.size = 2) 
   })
+  # Plot density chart
+  output$density_chart<-renderPlot({
+    ggplot(data(),aes_(x =input$num_compare, fill = ~Attrition)) + 
+                  geom_density(alpha = 0.7) + 
+                  scale_fill_manual(values = c("#386cb0","#fdb462"))
+  })
+  
+  # Load model
+  # load the model
+  model_path <- "GBM_1_AutoML_20210204_095214"
+  h2o_model <- h2o.loadModel(model_path)
+  
+  output$variable_important<-renderPlot({
+    h2o.varimp_plot(h2o_model)
+  })
+  output$shap_summary_plot<-renderPlot({
+    h2o.shap_summary_plot(h2o_model,test_h2o)
+  })
+  
 }
 
 
